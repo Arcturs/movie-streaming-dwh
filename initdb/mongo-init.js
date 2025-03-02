9416cb736b570ff8cb63f4e6943d07e533a00267
a.sashina <yaninastya2010@yandex.ru>
2025-03-02 23:23:25 +0300

    Расширено количество свойств сущностей

--- initdb/mongo-init.js
+++ initdb/mongo-init.js
@@ -5,12 +5,18 @@ db.movie_collection.insertMany([
     {
         _id: "test-1",
         title: "test",
-        release_date: "07-07-2020",
+        release_date: "2020-07-07",
         description: "test",
         duration: 120,
         genres: ["Adventure", "Detective"],
         director: "Test Director",
-        actors: ["Test Actor"]
+        actors: ["Test Actor"],
+        country: "USA",
+        subtitles: ["eng", "rus"],
+        audio: ["rus", "eng"],
+        nominations: ["Oscar Best Film 2021"],
+        awards: ["Oscar Best Film 2021"],
+        interesting_facts: ["fact 1", "fact 2"]
     }
 ]);
 
@@ -21,7 +27,12 @@ db.user_collection.insertMany([
         name: "test",
         email: "test@gmail.com",
         subscription: "true",
-        favourite_movies: ["test"]
+        favourite_movies: ["test"],
+        surname: "test",
+        birth_date: "2001-06-04",
+        created_at: "2021-08-12 13:38:35.340",
+        country: "India",
+        phone: "+79207639738"
     }
 ]);
 
@@ -30,7 +41,9 @@ db.actor_collection.insertMany([
     {
         _id: "test-1",
         name: "Test Actor",
-        starred_movies: ["test"]
+        starred_movies: ["test"],
+        birth_date: "1998-09-12",
+        country: "Russia"
     }
 ]);
 
@@ -39,6 +52,9 @@ db.director_collection.insertMany([
     {
         _id: "test-1",
         name: "Test Director",
-        directed_movies: ["test"]
+        directed_movies: ["test"],
+        nominations: ["nomination1"],
+        birth_date: "1998-09-12",
+        country: "Russia"
     }
 ]);
